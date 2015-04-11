8f9286e8bcbb219eb680939912d7cb683147f846
Benjamin Raethlein <benjamin.raethlein@sap.com>
2015-04-11 17:40:45 +0200

    first dialog version

--- scripts/module.js
+++ scripts/module.js
@@ -18,7 +18,7 @@ angular.module('praxismarket', ['ngMaterial'])
                 });
         };
     }).
-    controller('cardController', function ($scope) {
+    controller('cardController', function ($scope, $mdDialog) {
         $scope.joboffers = [
             {
                 'company': 'Foo',
@@ -29,4 +29,63 @@ angular.module('praxismarket', ['ngMaterial'])
             },
             {'company': 'Blub', 'title': 'Hihi', 'location': 'Berlin', 'firstname': 'Susanne', 'lastname': 'Sonne'}
         ];
+
+        $scope.showCompanyDetails = function(ev, companyId){
+            var companies = [{ 'name': 'FOOBAR COMPANY',
+                'description': 'Lorem Ipsum',
+                'website': 'http://drop.social',
+                'street': 'Philippstr.',
+                'city': 'Karlsruhe',
+                'numberOfEmployees': '9',
+                'country': 'Germany'}];
+            var company = companies[companyId];
+            $scope.companyDetails = {
+                'name': company.name,
+                'description': company.description,
+                'website': company.website,
+                'street': company.street,
+                'city': company.city,
+                'numberOfEmployees': company.numberOfEmployees,
+                'country': company.country
+            }
+
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'companyDetails.template.html',
+                targetEvent: ev,
+                resolve: {company: function(){ return company;}}
+            })
+                .then(function(answer) {
+                    $scope.alert = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.alert = 'You cancelled the dialog.';
+                });
+        }
+
+        $scope.showAdvanced = function(ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'companyDetails.template.html',
+                targetEvent: ev
+            })
+                .then(function(answer) {
+                    $scope.alert = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.alert = 'You cancelled the dialog.';
+                });
+        };
+
+        function DialogController($scope, $mdDialog, company) {
+            $scope.company = company;
+
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function() {
+                $mdDialog.cancel();
+            };
+            $scope.answer = function(answer) {
+                $mdDialog.hide(answer);
+            };
+        }
     });
