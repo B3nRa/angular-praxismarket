78c358f63b3a7786e8f64bfc61da779c3e35b3ad
Benjamin Raethlein <benjamin.raethlein@sap.com>
2015-04-11 14:47:13 +0200

    first model bind approaches

--- scripts/module.js
+++ scripts/module.js
@@ -1,4 +1,7 @@
 angular.module('praxismarket', [])
 .controller('cardController', function($scope){
-
+        $scope.joboffers = [
+            {'company': 'Foo', 'title': 'Bar', 'location': 'Karlsruhe'},
+            {'company': 'Blub', 'title': 'Hihi', 'location': 'Berlin'}
+        ];
     });
