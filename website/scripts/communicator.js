56e346e4f00db3da44429fce7698ece3527da96d
BenRa <benjamin.raethlein@gmail.com>
2015-04-12 18:29:42 +0200

    hardcoded offertypes are replaced with server ones, when getting correct answer.

--- website/scripts/communicator.js
+++ website/scripts/communicator.js
@@ -5,33 +5,33 @@
         serviceUrl: "http://praxis-market.appspot.com/?url=",
         getAllOfferTypes: function (cb) {
 
-            //var url = internal.serviceUrl + "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offertypes/all"
-            //var request = new XMLHttpRequest();
-            //request.open('GET', url, true);  // `false` makes the request synchronous
-            //request.onload = function () {
-            //    var jsonObj = JSON.parse(request.responseText);
-            //    for (var i in jsonObj) {
-            //        switch (jsonObj[i].id) {
-            //            case 3:
-            //                jsonObj[i].icon = "fa-wrench";
-            //                break;
-            //            case 5:
-            //                jsonObj[i].icon = "fa-line-chart";
-            //                break;
-            //            case 6:
-            //                jsonObj[i].icon = "fa-book";
-            //                break;
-            //            case 9:
-            //                jsonObj[i].icon = "fa-graduation-cap";
-            //                break;
-            //            default:
-            //                jsonObj[i].icon = "fa-info";
-            //                break;
-            //        }
-            //    }
-            //    cb(jsonObj);
-            //}
-            //request.send(null);
+            var url = internal.serviceUrl + "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offertypes/all"
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);  // `false` makes the request synchronous
+            request.onload = function () {
+                var jsonObj = JSON.parse(request.responseText);
+                for (var i in jsonObj) {
+                    switch (jsonObj[i].id) {
+                        case 3:
+                            jsonObj[i].icon = "fa-wrench";
+                            break;
+                        case 5:
+                            jsonObj[i].icon = "fa-line-chart";
+                            break;
+                        case 6:
+                            jsonObj[i].icon = "fa-book";
+                            break;
+                        case 9:
+                            jsonObj[i].icon = "fa-graduation-cap";
+                            break;
+                        default:
+                            jsonObj[i].icon = "fa-info";
+                            break;
+                    }
+                }
+                cb(jsonObj);
+            }
+            request.send(null);
 
             var offerTypes = [
                 {"id":9,"name":"Abschlussarbeit","shortname":"thesis", "icon": "fa-graduation-cap"},
